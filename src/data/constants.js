28984fc8ceba37b61d4d8a2427d928e97cf7ed2e
Roshan Mishra <157090530+Roshan2115@users.noreply.github.com>
2024-11-17 18:14:11 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning, I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Roshan2115",
   resume:
-    "https://drive.google.com/file/d/17n6cWj__vN55z0CkoXpJC3DCq7v_HMRT/view?usp=sharing",
+    "https://drive.google.com/file/d/1GIEiFzFw4f4ezqJjRFWuYCIEbH4T6SiS/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/roshan-mishra-1091a0243/"
 };
 
